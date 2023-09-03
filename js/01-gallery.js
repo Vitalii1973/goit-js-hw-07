407aed52c03a2d3e2b11e7c875e3e79cef410a5e
Vitalii1973 <protsyukv@ukr.net>
2023-09-03 23:29:08 +0300

    goit-js-hw-07 -2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,10 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 console.log(galleryItems);
 
 const galleryList = document.querySelector(".gallery");
 
 function createGalleryItem(item) {
-  // Створюємо елементи для кожного елемента галереї
   const galleryItem = document.createElement("li");
   galleryItem.classList.add("gallery__item");
 
@@ -20,7 +18,6 @@ function createGalleryItem(item) {
   image.alt = item.description;
   image.setAttribute("data-source", item.original);
 
-  // З'єднуємо елементи у структуру
   link.appendChild(image);
   galleryItem.appendChild(link);
 
@@ -28,7 +25,6 @@ function createGalleryItem(item) {
 }
 
 const galleryMarkup = galleryItems.map(createGalleryItem);
-// Додаємо створені елементи галереї до списку
 galleryList.append(...galleryMarkup);
 
 galleryList.addEventListener("click", (event) => {
@@ -40,15 +36,25 @@ galleryList.addEventListener("click", (event) => {
 
   const source = event.target.dataset.source;
 
-  const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(
+    `
     <img src="${source}" width="800" height="600">
-  `);
+  `,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", handleKeyDown);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", handleKeyDown);
+      },
+    }
+  );
 
   instance.show();
-});
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape") {
-    basicLightbox.close();
+  function handleKeyDown(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
   }
 });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,47 +1,47 @@
-// Імпорт масиву galleryItems з файлу gallery-items.js.
-
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-
-// Вивід масиву galleryItems в консоль для перевірки.
 
 console.log(galleryItems);
-// Отримуємо посилання на елемент списку ul.gallery у DOM-дереві.
 
 const galleryList = document.querySelector(".gallery");
 
-// Створення функції createGalleryItem, яка буде використовуватися для створення розмітки для кожного елемента галереї.
-
 function createGalleryItem(item) {
-  // Створюємо елемента <li> для елемента галереї
   const galleryItem = document.createElement("li");
   galleryItem.classList.add("gallery__item");
 
-  // Створюємо елементи <a> для зображення
   const link = document.createElement("a");
   link.classList.add("gallery__link");
   link.href = item.original;
-  link.setAttribute("data-lightbox", "image-set");
 
-  // Створюємо елементи <img> для зображення
   const image = document.createElement("img");
   image.classList.add("gallery__image");
   image.src = item.preview;
   image.alt = item.description;
-  image.setAttribute("data-title", item.description);
+  image.setAttribute("data-source", item.original);
 
-  // Додаємо елементи до DOM-структури
   link.appendChild(image);
   galleryItem.appendChild(link);
 
   return galleryItem;
 }
-// Створюємо масив galleryMarkup, який містить створені елементи галереї за допомогою функції createGalleryItem.
+
 const galleryMarkup = galleryItems.map(createGalleryItem);
 galleryList.append(...galleryMarkup);
 
-// Ініціалізовуємо бібліотеку SimpleLightbox
+// Ініціалізація бібліотеки SimpleLightbox
 const lightbox = new SimpleLightbox(".gallery__link", {
-  captions: true, // Увімкнули підписи
-  captionDelay: 250, // Затримка перед показом підпису
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  onShow: (instance) => {
+    document.addEventListener("keydown", handleKeyDown);
+    instance.on("close", () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    });
+  },
 });
+
+function handleKeyDown(event) {
+  if (event.key === "Escape") {
+    lightbox.close();
+  }
+}
